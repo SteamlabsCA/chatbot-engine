f6160ca87020ce3b7a3dae7baab72063e2482d7d
Glitch (hello-webpage) <none>
2021-02-04 19:50:30 +0000

    🎻🌦 Checkpoint ./script.js:7451474/791

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
-/* If you're feeling fancy you can add interactivity 
-    to your site with Javascript */
+//Add your custom responses here
 
-// prints "hi" in the browser's dev tools console
-console.log("hi");
+var responses = [
+  "Jeez, for a guy with a ton of clocks, you sure don't pay much attention to time.",
+  "Show me the money!"
+];
+
+// Choose some movie characters to include
+
+
+// Include some AI generated text from GPT-2
+
+
+// Add some scripted answers to specific questions
+
+customQuestion = "What time is it?";
+customAnswer = Date.now();
+
+
