7f4bed418a31b479332349361d9b79c5d28361a5
Glitch (hello-webpage) <none>
2021-02-10 19:15:03 +0000

    ✊🤘 Checkpoint ./Striker.txt:7451474/2603 ./scripts/Striker.txt:7451474/166 ./script.js:7451474/121

--- script.js
+++ script.js
@@ -1,35 +1,41 @@
 
+
+function pickResponse(inputPrompt) {
+  
+}
+
+
 function initData() {
-//Add your custom responses here
+  //Add your custom responses here
 
-var responses = [
-  "Jeez, for a guy with a ton of clocks, you sure don't pay much attention to time.",
-  "Show me the money!",
-  "I'm late!",
-  "xckjfh jskdh"
-];
+  var responses = [
+    "Jeez, for a guy with a ton of clocks, you sure don't pay much attention to time.",
+    "Show me the money!",
+    "I'm late!",
+    "xckjfh jskdh"
+  ];
 
-// Choose some movie characters to include
-var characters = [
-  "airplane: striker",
-  "airplane: Elaine"
-]
+  // Choose some movie characters to include
+  var characters = [
+    "airplane: striker",
+    "airplane: Elaine"
+  ]
 
-// Include some AI generated responses from GPT-2 based on some question prompts. Change these prompts to your liking
+  // Include some AI generated responses from GPT-2 based on some question prompts. Change these prompts to your liking
 
-for (var i = 0; i < 5; i++) {
-  responses.push(GPT2Generate("What should we order for dinner?"));
-}
+  for (var i = 0; i < 5; i++) {
+    responses.push(GPT2Generate("What should we order for dinner?"));
+  }
 
-for (var i = 0; i < 5; i++) {
-  responses.push(GPT2Generate("What do you think of this t-shirt?"));
-}
+  for (var i = 0; i < 5; i++) {
+    responses.push(GPT2Generate("What do you think of this t-shirt?"));
+  }
 
 
-// Add some scripted answers to specific questions
+  // Add some scripted answers to specific questions
 
-var customQuestion = "What time is it?";
-var customAnswer = Date.now();
+  var customQuestion = "What time is it?";
+  var customAnswer = Date.now();
 }
 
 function GPT2Generate(textPrompt) {
