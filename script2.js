b9dab7f6c823d637acc6a529ff7de23300b493d2
Glitch (hello-webpage) <none>
2021-02-10 19:38:06 +0000

    👜🍫 Checkpoint ./script.js:7451474/1026 ./script2.js:7451474/1540 ./script3.js:7451474/1071 ./index.html:7451474/380 ./Austin.txt:7451474/9747

---
+++ script2.js
@@ -0,0 +1,66 @@
+
+
+function pickResponse(inputPrompt) {
+  var responseList = "Striker.txt";
+  
+  if (detectIntent(inputPrompt, "Happy")) {
+    var responseList = "Austin.txt";
+  }
+  
+  responseList = fillInTheBlanks(inputPrompt, responseList);
+  
+  return bestResponse(inputPrompt, responseList);
+}
+
+function fillInTheBlanks (inputPrompt, responseList) {
+  // Use GPT-2 AI to fill in the blanks in the responselist with appropriate words
+  
+}
+
+function detectIntent(inputPrompt, emotion) {
+  
+}
+
+function bestResponse(inputPrompt, responseList) {
+  
+}
+
+
+function initData() {
+  //Add your custom responses here
+
+  var responses = [
+    "Jeez, for a guy with a ton of clocks, you sure don't pay much attention to time.",
+    "Show me the money!",
+    "I'm late!",
+    "xckjfh jskdh"
+  ];
+
+  // Choose some movie characters to include
+  var characters = [
+    "airplane: striker",
+    "airplane: Elaine"
+  ]
+
+  // Include some AI generated responses from GPT-2 based on some question prompts. Change these prompts to your liking
+
+  for (var i = 0; i < 5; i++) {
+    responses.push(GPT2Generate("What should we order for dinner?"));
+  }
+
+  for (var i = 0; i < 5; i++) {
+    responses.push(GPT2Generate("What do you think of this t-shirt?"));
+  }
+
+
+  // Add some scripted answers to specific questions
+
+  var customQuestion = "What time is it?";
+  var customAnswer = Date.now();
+}
+
+function GPT2Generate(textPrompt) {
+  // Call GPT2 API
+  
+}
+
