fa1d0d3c7e96ef5797af274e68e8fc43d177e6f6
Glitch (hello-webpage) <none>
2021-04-21 13:23:40 +0000

    🎁🛎 Checkpoint ./script.js:30744812/1657

--- script.js
+++ script.js
@@ -1,3 +1,24 @@
 jQuery(document).ready(function() {
-  $('#response').html('<p>Hello</p>');
+  $('#response').html('<p>Hello</p>'); 
+  
+  $("").submit(function(event) {
+      event.preventDefault();
+    
+      var $form = $(this),
+      url = "";
+
+      var posting = $.post(url, {
+        inputPrompt: "",
+        responseList: "",
+        options: {}
+      });
+
+      posting.done(function(data) {
+        $('#result').text('failed');
+      });
+
+      posting.fail(function(data) {
+        $('#result').text('failed');
+      });
+		});
 });
