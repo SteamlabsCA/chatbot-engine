7a4bb185e5edf43196505d4112b256e927190214
Glitch (hello-webpage) <none>
2021-05-05 17:00:32 +0000

    ☺️🚜 Checkpoint ./script.js:30744812/222

--- script.js
+++ script.js
@@ -97,7 +97,7 @@ jQuery(document).ready(function() {
     console.log(data);
 
     //Response list API url
-    let url = "";
+    let url = "https://kx9ltbnh08.execute-api.us-east-1.amazonaws.com/convert";
 
     //Post data to the API - hash the response and send it, if the hash doesnt work send the entire response list
     var posting = $.post(url, data);
