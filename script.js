f3e465e7a742ae6b88a282830141c4759ae92a5a
Glitch (hello-webpage) <none>
2021-05-10 17:23:38 +0000

    🎎🐄 Checkpoint ./script.js:28176799/159

--- script.js
+++ script.js
@@ -99,7 +99,7 @@ jQuery(document).ready(function() {
       responseList: ["I like pie","Roasted goat is nice but takes a long time to <cook>","a fresh fruit bowl sounds <nice>"]
     }
     
-    let url = ' http://a2e00f367a1e64bcb8cee16789c2d8b8-6cbf9dd8014b84e1.elb.us-east-1.amazonaws.com/convert';
+    let url = 'https://hc67ns0rf2.execute-api.us-west-2.amazonaws.com/dev/convert';
     
     console.log(data);
 
