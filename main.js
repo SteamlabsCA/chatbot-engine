0427444f5a608c2f027ce17322079ed4011869db
Glitch (hello-webpage) <none>
2021-05-12 14:47:30 +0000

    ⛷👦 Checkpoint ./main.js:30744812/79

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 /* globals responseList */
 
 jQuery(document).ready(function(){
+  // Change these files to change the response list text from the assets
   responseList(["testText.txt","testText2.txt"]);
 });
