40b3a8eb8167234e23ad68c5a42f7f792d50b88f
Glitch (hello-webpage) <none>
2021-05-19 20:57:07 +0000

    😜🌋 Checkpoint ./script.js:30744812/1772

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ function responseList(textArray) {
   $.when
     .apply($,textArray.map(function(url) {
         return $.ajax({
-          url: "responseList/",
+          url: "assets/",
           dataType: "text"
         });
       }))
@@ -18,7 +18,7 @@ function responseList(textArray) {
         responseListArr = result[0].split("\n");
         responseListArr.pop();
         responseListArr.map(function(file) {
-          let $checkbox = "<span id='checkbox'><input type='checkbox' id="+file+" name="+file+" value="+file+" ><label for="+file+" >"+file+" </label></span>";
+          let $checkbox = "<span id='checkboxes'><input type='checkbox' id="+file+" name="+file+" value="+file+" ><label for="+file+" >"+file+" </label></span>";
           $("#script_choice").append($checkbox);
         });
     })
@@ -67,10 +67,30 @@ function responseList(textArray) {
   //----Start: Pick Response----
   function pickResponse(inputPrompt) {
     var selected = [];
+    let ress;
     $('#checkboxes input:checked').each(function() {
         selected.push($(this).attr('name'));
     });
-    console.log(responseListArr);
+    console.log(selected)
+    
+    $.when
+    .apply($,selected.map(function(url) {
+        return $.ajax({
+          url: "assets/"+ url,
+          dataType: "text"
+        });
+      }))
+    .done(function() {
+      for (var i = 0; i < arguments.length; i++) {
+        // ress[i] = arguments[i][0];
+        console.log(arguments)
+      }
+    })
+    .fail(function(error) {
+      console.log("Text File Retrieval Error: " + error);
+    });
+    
+    
     
     //Load the data to be sent to the API
     // let data = {
@@ -85,53 +105,52 @@ function responseList(textArray) {
       language: "EN"
     }
     
-    // let url = 'https://57sunxdj45.execute-api.us-west-2.amazonaws.com/dev/convert';
-    let url ='';
+//     let url = 'https://57sunxdj45.execute-api.us-west-2.amazonaws.com/dev/convert';
 
-    //Post data to the API - hash the response and send it, if the hash doesnt work send the entire response list
-    var posting = $.ajax({
-                      url: url,
-                      type: "POST",
-                      contentType: "application/json",
-                      data: JSON.stringify(data),
-                  });
+//     //Post data to the API - hash the response and send it, if the hash doesnt work send the entire response list
+//     var posting = $.ajax({
+//                       url: url,
+//                       type: "POST",
+//                       contentType: "application/json",
+//                       data: JSON.stringify(data),
+//                   });
     
-    posting.done(function(responseData) {
-      if (responseData === -1) {
-        console.log("rejected")
-        //If server doesn’t have that list cached
-        let newData = {
-          inputPrompt: inputPrompt,
-          responseList: responseListConcat,
-          language: "EN"
-        };
+//     posting.done(function(responseData) {
+//       if (responseData === -1) {
+//         console.log("rejected")
+//         //If server doesn’t have that list cached
+//         let newData = {
+//           inputPrompt: inputPrompt,
+//           responseList: responseListConcat,
+//           language: "EN"
+//         };
 
-        var posting = $.post(url, newData);
+//         var posting = $.post(url, newData);
 
-        posting.done(function(data) {
-          if (data) {
-            for (let i of data) {
-              let $bot_response = "<li class='bot_response'><img src='https://cdn.glitch.com/a1898aab-94e6-4c8f-8dd2-5de4e5ff6a2b%2FSteamLabs_Monogram_RGB_Black.png?v=1619620318564' class='bot_profile'></img><span class='content_container'><span class='name_date'><h3>Bot</h3><p>"+date.toLocaleTimeString() + "</p></span><p>"+ i.response + ": " + i.topscore + "</p></span></li>";
-              $(".chat_response").append($bot_response);
-            }
-          } else {
-            console.log("response failed");
-          }
-        });
+//         posting.done(function(data) {
+//           if (data) {
+//             for (let i of data) {
+//               let $bot_response = "<li class='bot_response'><img src='https://cdn.glitch.com/a1898aab-94e6-4c8f-8dd2-5de4e5ff6a2b%2FSteamLabs_Monogram_RGB_Black.png?v=1619620318564' class='bot_profile'></img><span class='content_container'><span class='name_date'><h3>Bot</h3><p>"+date.toLocaleTimeString() + "</p></span><p>"+ i.response + ": " + i.topscore + "</p></span></li>";
+//               $(".chat_response").append($bot_response);
+//             }
+//           } else {
+//             console.log("response failed");
+//           }
+//         });
 
-        posting.fail(function(data) {
-          console.log("Posting Full Response List Failed");
-        });
-      }else {
-        let $bot_response = "<li class='bot_response'><img src='https://cdn.glitch.com/a1898aab-94e6-4c8f-8dd2-5de4e5ff6a2b%2FSteamLabs_Monogram_RGB_Black.png?v=1619620318564' class='bot_profile'></img><span class='content_container'><span class='name_date'><h3>Bot</h3><p>"+date.toLocaleTimeString() + "</p></span><p>"+ responseData+ "</p></span></li>";
-        $(".chat_response").append($bot_response);
-        (document.getElementById("response")).scrollTop = (document.getElementById("response")).scrollHeight;
-      }
-    });
+//         posting.fail(function(data) {
+//           console.log("Posting Full Response List Failed");
+//         });
+//       }else {
+//         let $bot_response = "<li class='bot_response'><img src='https://cdn.glitch.com/a1898aab-94e6-4c8f-8dd2-5de4e5ff6a2b%2FSteamLabs_Monogram_RGB_Black.png?v=1619620318564' class='bot_profile'></img><span class='content_container'><span class='name_date'><h3>Bot</h3><p>"+date.toLocaleTimeString() + "</p></span><p>"+ responseData+ "</p></span></li>";
+//         $(".chat_response").append($bot_response);
+//         (document.getElementById("response")).scrollTop = (document.getElementById("response")).scrollHeight;
+//       }
+//     });
 
-    posting.fail(function(data) {
-      console.log("Posting Hashed Response List Failed:" + data);
-    });
+//     posting.fail(function(data) {
+//       console.log("Posting Hashed Response List Failed:" + data);
+//     });
   }
   //----End: Pick Response----
 
