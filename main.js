bb42e26164868348a091b56cf189cd3c8994b5fd
Glitch (chatbot-engine-test) <none>
2021-05-31 15:38:52 +0000

    🚵⚽️ Checkpoint ./main.js:30744812/36

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 /* globals responseList */
 jQuery(document).ready(function(){
   $("#start").click(function() {
+    // Insert to API key here
     responseList("wR5uUCYIL21JQxPAAJAL14VVFh62YbHD4Kjdn7om");
     $("#start_container").fadeOut("fast",function(){
       $(".activity").fadeIn("fast");
