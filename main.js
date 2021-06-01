df1200ed74e90771fa2061cf9109633ea48bb490
DenEdwards <denzelsedwards@gmail.com>
2021-06-01 15:22:21 -0400

    Update ReadMe and Landing Page

--- main.js
+++ main.js
@@ -1,17 +1,17 @@
 /* globals responseList */
 
 // Insert teacher API key here
-let teacherKey = '';
+let teacherKey = "";
 
-jQuery(document).ready(function () {
-	$('#start').click(function () {
-		if (teacherKey.length > 0) {
-			responseList(teacherKey);
-			$('#start_container').fadeOut('fast', function () {
-				$('.activity').fadeIn('fast');
-			});
-		} else {
-			alert('Missing Teacher API key');
-		}
-	});
+jQuery(document).ready(function() {
+  $("#start").click(function() {
+    if (teacherKey.length > 0) {
+      responseList(teacherKey);
+      $("#start_container").fadeOut("fast", function() {
+        $(".activity").fadeIn("fast");
+      });
+    } else {
+      alert("Missing Teacher API key");
+    }
+  });
 });
