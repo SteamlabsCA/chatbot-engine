7f119ffe7f6e81d32b551d168a4335ff8d6f0d29
Glitch (chatbot-engine-test) <none>
2021-06-01 20:27:59 +0000

    🚔📰 Checkpoint ./script.js:30744812/29605 ./main.js:30744812/40

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 /* globals responseList */
 
 // Insert teacher API key here
-let teacherKey = "wR5uUCYIL21JQxPAAJAL14VVFh62YbHD4Kjdn7om";
+let teacherKey = "";
 
 jQuery(document).ready(function() {
   $("#start").click(function() {
