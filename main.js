65883d610eee141bd1c2b792dcf09979565b8726
DenEdwards <denzelsedwards@gmail.com>
2021-06-02 14:41:06 -0400

    Quick Update

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 /* globals bestResponse */
 
 // Insert teacher API key here
-let teacherKey = 'wR5uUCYIL21JQxPAAJAL14VVFh62YbHD4Kjdn7om';
+let teacherKey = '';
 let responseList = '*'; //Chooses all scripts
 // let responseList = ""; //lets GUI decide
 // let responseList = "Woody.txt"; //Choose based off text
