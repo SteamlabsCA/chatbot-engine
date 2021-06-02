c00e5cb489682932fcb6414cac6ba8ad16686ce2
Glitch (chatbot-engine-test) <none>
2021-06-02 18:44:37 +0000

    🛥🐩 Checkpoint ./lines/Your Characters/Woody.txt:30744812/22191 ./lines/Your Characters/character2.txt:30744812/12 ./main.js:30744812/72

--- main.js
+++ main.js
@@ -2,9 +2,9 @@
 
 // Insert teacher API key here
 let teacherKey = "wR5uUCYIL21JQxPAAJAL14VVFh62YbHD4Kjdn7om";
-let responseList = "*";
-// let responseList = "";
-// let responseList = "Woody.txt";
+let responseList = "*"; //Chooses all scripts
+// let responseList = ""; //lets GUI decide
+// let responseList = "Woody.txt"; //Choose based off text
 
 function botResponse() {
   bestResponse(teacherKey, responseList);
