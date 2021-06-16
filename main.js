cd80c9b4b7f440c60148b97eb56be0a5c9f52ccc
Glitch (chatbot-engine-test) <none>
2021-06-16 14:16:07 -0400

    Update for Style

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 /* globals bestResponse detectMood detectIntent detectMode responseList fillInTheBlanks*/
 
 // Insert teacher API key here
-let teacherKey = "wR5uUCYIL21JQxPAAJAL14VVFh62YbHD4Kjdn7om";
+let teacherKey = "";
 // let responseListOrg = "*"; //Chooses all scripts
 // let responseListOrg = ""; //lets GUI decide
 let responseListOrg = "Woody.txt"; //Choose based off text
