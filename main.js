eebc8c9cbbdcdd39e11a747b2ab09933f2235ea7
Glitch (chatbot-engine-test) <none>
2021-08-09 14:38:20 -0400

    language UX update

--- main.js
+++ main.js
@@ -1,54 +1,33 @@
-
 // Insert teacher API key here
-let teacherKey = "";
+let teacherKey = 'wR5uUCYIL21JQxPAAJAL14VVFh62YbHD4Kjdn7om';
 
-let responseListOrg = "*"; //Chooses all scripts
+//EN-English, FR-French, NO-Norwegian
+let language = 'NO';
+
+// let responseListOrg = "*"; //Chooses all scripts
 // let responseListOrg = ""; //lets GUI decide
-// let responseListOrg = "Woody.txt"; //Choose based off text
+let responseListOrg = 'Woody.txt'; //Choose based off text
 
+async function botResponse(inputPrompt) {
+	// if (await detectMood(inputPrompt, "Happy")) {
+	//   responseList = "character2.txt";
+	// }
 
-async function botResponse(inputPrompt){
-// if (await detectMood(inputPrompt, "Happy")) {
-//   responseList = "character2.txt";
-// }
-  
-//   let response = await detectIntent(inputPrompt, ["rent a car","rent a bike","book a flight"]) 
-//   if (response === "rent a car") {
-//     responseList = "car.txt";
-//   }
-  
-//   if (response === "rent a bike") {
-//     responseList = "bike.txt";
-//   }
-  
-//   if (response === "book a flight") {
-//     responseList = "flight.txt";
-//   }
-  
-  fillInTheBlanks();
-  return await bestResponse(inputPrompt, teacherKey, responseList)
+	//   let response = await detectIntent(inputPrompt, ["rent a car","rent a bike","book a flight"])
+	//   if (response === "rent a car") {
+	//     responseList = "car.txt";
+	//   }
+
+	//   if (response === "rent a bike") {
+	//     responseList = "bike.txt";
+	//   }
+
+	//   if (response === "book a flight") {
+	//     responseList = "flight.txt";
+	//   }
+
+	fillInTheBlanks();
+	return await bestResponse(inputPrompt, teacherKey, responseList, language);
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 /* globals bestResponse detectMood detectIntent detectMode responseList fillInTheBlanks*/
